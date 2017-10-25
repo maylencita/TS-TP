41c4af1638697a6684d2da585ba554ae99171724
Bluesheeptoken <louis.fruleux1@gmail.com>
2017-10-25 15:43:13 +0200

    Creation du modele

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
 console.log('Application started!')
 
+type Utilisateur = {
+    pseudo: String,
+    status: Status,
+    points: Number
+}
+
+type Status = 
+    Connecte  |
+    Deconnecte|
+    Suspendu
+
+type Connecte = "Connecte"
+
+type Deconnecte = "Deconnecte"
+
+type Suspendu = "Suspendu"
+
+type Message = 
+    Question  |
+    Reponse   |
+    Note
+
+type Question = {
+    contenu: String
+}
+
+type Reponse = {
+    contenu: String
+}
+
+type Note = {
+    note: Number
+}
+
+type Channel = {
+    nom: String,
+    createur: Utilisateur,
+    listeParticipant: Number[]
+}
+
+type Serveur = {
+    listeUtilisateur: Utilisateur[],
+    listeChannel: Channel[],
+    listeMessage: Message[]
+
+}
+
 //--------------------------
 // TESTING THE APPLICATION
 //-------------------------
