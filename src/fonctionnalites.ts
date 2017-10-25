59b497287b74f4475d2f3ed9a6b41e79ad0e7e2e
JoseAlejandro <josesa10_10@hotmail.com>
2017-10-25 16:26:19 +0200

    fonctionnalites

---
+++ src/fonctionnalites.ts
@@ -0,0 +1,127 @@
+function initServeur(){
+	const user:Utilisateur = {pseudo:superCodeur, status:"Deconnecte", points:5};
+	const serveur:Serveur = {utilisateurs:[user], channels:[], messages:[]};
+	return serveur;
+}
+
+function nouvelUtilisateur(serveur:Serveur, pseudo:String){
+	const user:Utilisateur = {pseudo:pseudo, status: "Deconnecte", points:0};
+	serveur.utilisateurs.push(user);
+	return serveur;
+}
+
+function connectUtilisateur(user:Utilisateur, serveur:Serveur)
+{
+	var index:number = seveur.utilisateurs.indexOf(user);
+	if(index!==-1)
+	{
+		serveur.utilisateurs[index].status="Connecte";
+	}
+	return serveur;
+}
+
+function creerChannel(serveur:Serveur, user: Utilisateur, nom:String)
+{
+	if(user.points>=1)
+	{
+		const channel:Channel = {nom:nom, createur:user, participants:[user]};
+		serveur.channels.push(channel);
+	}
+	return serveur; 
+}
+
+function ajouterUtilisateurChannel(serveur:Seveur, user:Utilisateur, newUser:Utilisateur, channel:Channel)
+{
+	var index:number = channel.participants.indexOf(user);
+	var indexChannel:number = serveur.channels.indexOF(channel);
+	if(index!==-1 && (channel.createur===user||user.points>=2))
+	{
+		serveur.channels[indexChannel].participants.push(newUser);
+	}
+	return serveur;
+}
+
+function lireMessagesChannel(serveur:Serveur, user:Utilisateur, channel:Channel)
+{
+	let messages:Message[]  = []; 
+	if(channel.participants.indexOf(user)!==-1)
+	{
+		for(id:number in channel.messages)
+		{
+			messages.push(serveur.messages[id]);
+		}
+	}
+	return messages;
+}
+
+function envoyerQuestion(serveur:Serveur, user:Utilisateur, contenu:String, channels:Channel[])
+{
+	if(channel.participants.indexOf(user)!==-1)
+	{
+		if(serveur.utilisateurs[serveur.utilisateurs.indexOf(user)].points===0)
+		{
+			serveur.utilisateurs[serveur.utilisateurs.indexOf(user)].points++;
+		}
+		const question:Question = {id:serveur.messages.length,contenu:contenu, emetteur:user, destination:channels};
+		serveur.messages.push[question]
+		for(channel:Channel in channels)
+		{
+			serveur.channels[seveur.channels.indexOf(channel)].messages.push(question.id);
+		}
+	}
+	return serveur;
+}
+
+function repondreQuestion(serveur:Serveur, user:Utilisateur, contenu:String, question:number, channel:Channel)
+{
+	if((channel.participants.indexOf(user)!==-1 && user.points>=3)||user.points===5)
+	{
+		const reponse:Reponse = {id:serveur.messages.length,contenu:contenu, emetteur:user, idQuestion:question};
+		serveur.messages.push[reponse];
+		serveur.channels[seveur.channels.indexOf(channel)].messages.push(reponse.id);
+	}
+	return serveur;
+}
+
+function noterQuestion(serveur:Serveur, user:Utilisateur, contenu:number, source:number, channel:Channel)
+{
+	if(channel.participants.indexOf(user)!==-1 && user.points>=1)
+	{
+		const note:Note = {id:serveur.messages.length,contenu:contenu, emetteur:user, idSource:source};
+		serveur.messages.push[note];
+		serveur.channels[seveur.channels.indexOf(channel)].messages.push(note.id);
+		calculerPointsUtilisateurs(serveur,user);
+	}
+	return serveur;
+}
+
+function noterReponse(serveur:Serveur, user:Utilisateur, contenu:number, source:number, channel:Channel)
+{
+	if(channel.participants.indexOf(user)!==-1 && user.points>=2)
+	{
+		const note:Note = {id:serveur.messages.length,contenu:contenu, emetteur:user, idSource:source};
+		serveur.messages.push[note];
+		serveur.channels[seveur.channels.indexOf(channel)].messages.push(note.id);
+		calculerPointsUtilisateurs(serveur,user);
+	}
+	return serveur;
+}
+
+function suspendreUtilisateur(serveur:Serveur, user:Utilisateur, userSuspendre:Utilisateur)
+{
+	if(user.points===5)
+	{
+		serveur.utilisateurs[serveur.utilisateurs.indexOF(userSuspendre)].status="Suspendu";
+	}
+	return serveur;
+}
+
+function calculerPointsUtilisateurs(serveur:Serveur, user:Utilisateur)
+{
+	var index:number = serveur.utilisateurs.indexOf(user);
+	if(serveur.utilisateurs[index].points<5)
+	{
+		serveur.utilisateurs[index].points++;
+	}
+	return serveur;
+}
--- src/types.ts
+++ src/types.ts
@@ -9,27 +9,31 @@ type Status = "Connecte" | "Deconnecte" | "Suspendu"
 type Message = Question | Reponse | Note
 
 type Question = {
+    id: number,
     contenu: string, 
     emetteur: Utilisateur, 
     destination: Channel[]
 }
 
 type Reponse = {
+    id: number,
     contenu: string, 
     emetteur: Utilisateur, 
-    question: Question
+    idQuestion: number
 }
 
 type Note = {
+    id: number,
     contenu: number, 
     emetteur: Utilisateur, 
-    source: Question | Reponse
+    idSource: number
 }
 
 type Channel = {
     nom: string, 
     createur: Utilisateur, 
-    participants: Utilisateur[]
+    participants: Utilisateur[],
+    messages: number[]
 }
 
 type Serveur = {
