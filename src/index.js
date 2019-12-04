c119aff909ec82e76928850a6a26a38b833493dd
François <francois.leguernic@gmail.com>
2019-12-04 15:32:03 +0100

    première modification

--- src/index.js
+++ src/index.js
@@ -1,79 +1,42 @@
 // import * as test from './tests'
 // import * as services from './services'
 // import * as models from './models'
-
 //--------------------------
 // MODEL
 //-------------------------
-
 // TODO complete
-
-class ServerState{
-  users: [User];
-  channels:[Channel];
-  messages:[Message];
-}
-
-class User {
-  pseudo: string;
-  status: Status;
-  points:number;
-
-}
-
-class Status {
-  status:String
-}
-
-class Message{
-  message:string;
-  reponse;string;
-  note:number;
-}
-
-class Channel {
-  nom:string;
-  créateur:User;
-  participants:[User]
-}
-
-
+var ServerState = /** @class */ (function () {
+    function ServerState() {
+    }
+    return ServerState;
+}());
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
 //--------------------------
 // SERVICES
 //-------------------------
-
-function initState(): ServerState {
-  const superUser: User = { pseudo: 'admin' } //status: 'Connected', points: 5
-
-  let newServer: ServerState = {
-    users: [superUser]
-  };
-
-  return newServer;
-};
-
+function initState() {
+    var superUser = { pseudo: 'admin' }; //status: 'Connected', points: 5
+    var newServer = {
+        users: [superUser]
+    };
+    return newServer;
+}
+;
 // function registerUser(state: ServerState, user: User): ServerState //We always pass server state
-
 // function createChannel //Crée un chanel sans participants. Vérifier points >= 1
-
 // function inviteUser //Vérifier user is owner ou points >= 2; Ajoute un utilisateur à un channel
-
 // readChannel //Vérifier appartennance 
-
 // sendQuestion //Vérifier appartennance; if points ==0 then add 1 point to user
-
 // sendAnswer //Vérifier appartennance ou super user && points >= 3
-
 // noteQuestion //Vérifier appartenance && points >= 1 ; récalculer points
-
 // noteAnswer //Vérifier appartenance && points >= 2 ; récalculer points
-
 // suspendUser // only super users (5 points) peuvent suspendre un utilisateur
-
 // calculerPoints // selon la notation de ces questions / responses
-
 //--------------------------
 // TESTING THE APPLICATION
 //-------------------------
-
 document.body.textContent = "Hello !";
